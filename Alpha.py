314f07d366e6bc335f3ae918eca3eb812482409f
sharjailani <47170584+sharjailani@users.noreply.github.com>
2019-02-12 14:20:01 +0530

    Create Alpha.py

---
+++ Alpha.py
@@ -0,0 +1,6 @@
+x=input()
+lit=["a","b","c","d","e","f","g","h","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+if x in lit:
+  print("Alphabet")
+else:
+  print("No")
