67d13b2e87e61e79cb7d92a0f8786ac6ea465528
sharjailani <47170584+sharjailani@users.noreply.github.com>
2019-02-15 14:08:01 +0530

    Update primei.py

--- primei.py
+++ primei.py
@@ -1,8 +1,8 @@
 lower,upper=input().split()
 lower,upper=int(lower),int(upper)
-for n in range(lower,upper):
+for n in range(lower+1,upper+1):
    if n > 1:
-       for i in range(2,n+1):
+       for i in range(2,int(n)):
            if (n % i) == 0:
                break
        else:
