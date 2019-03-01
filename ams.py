98b30d14f33943846b291de872c7b8ba6bb79417
sharjailani <47170584+sharjailani@users.noreply.github.com>
2019-03-01 12:49:15 +0530

    Create ams.py

---
+++ ams.py
@@ -0,0 +1,12 @@
+a,b =input().split() 
+a,b=int(a),int(b)
+for num in range(a, b + 1):
+   order = len(str(num))
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+   if num == sum:
+       print(num)
