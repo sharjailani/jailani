a18b245586a85d2484414366ef59e513e87fc393
sharjailani <47170584+sharjailani@users.noreply.github.com>
2019-03-06 23:45:04 +0530

    Update breakkyy.py

--- breakkyy.py
+++ breakkyy.py
@@ -2,7 +2,7 @@ string1,string2=input().split(' ')
 c=0
 for i in string1:
     if i in string2:
-        print('Yes')
+        print('yes')
         c+=1
         break
-if c==0:print('No')
+if c==0:print('no')
